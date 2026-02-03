b9778109c31db2803d217a6e58b8ca38e019091c
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2026-02-03 18:30:58 -0300

    feat: Teste da API

--- src/main/java/com/nadson/api_produtos/controller/HelloController.java
+++ src/main/java/com/nadson/api_produtos/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.nadson.api_produtos.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, World!";
+    }
 }
