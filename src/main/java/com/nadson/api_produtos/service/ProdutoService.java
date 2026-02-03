7f47d33f66c6f25d0da43cdd3c32e96eb6d8775d
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2026-02-03 16:59:24 -0300

    feat: Service de Produto

---
+++ src/main/java/com/nadson/api_produtos/service/ProdutoService.java
@@ -0,0 +1,32 @@
+package com.nadson.api_produtos.service;
+
+import com.nadson.api_produtos.model.Produto;
+import com.nadson.api_produtos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    public List<Produto> listarTodos(){
+        return repository.findAll();
+    }
+
+    public Optional<Produto> buscarPorId(Long id){
+        return repository.findById(id);
+    }
+
+    public Produto salvar(Produto produto){
+        return repository.save(produto);
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
