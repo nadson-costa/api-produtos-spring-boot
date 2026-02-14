80aa8f6c7f0d6bbe67b1f6cd859dbe8015c6581d
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2026-02-14 10:15:37 -0300

    feat: Implementação da classe de Error Response

---
+++ src/main/java/com/nadson/api_produtos/dto/ErrorResponse.java
@@ -0,0 +1,36 @@
+package com.nadson.api_produtos.dto;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+    private String timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(String timestamp, Integer status, String error, String message, String path){
+        this.timestamp = LocalDateTime.now().toString();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public String getTimeStamp(){
+        return timestamp;
+    }
+    public Integer getStatus(){
+        return status;
+    }
+    public String getError(){
+        return error;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public String getPath(){
+        return path;
+    }
+
+}
